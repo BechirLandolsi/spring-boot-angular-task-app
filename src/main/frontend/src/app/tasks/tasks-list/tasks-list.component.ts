a72c79f5802e743ad2f0a2db6e58183c746b1646
Roberto Moran <remorandev@gmail.com>
2019-01-21 23:23:10 +0100

    Mejora de mock y manejo de lista desde task-list.component.ts

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from "../task.model";
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
+    tasks: Task[] = [];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.tasks.push(new Task(1, "Task 1", true, "15/01/2019"));
+        this.tasks.push(new Task(1, "Task 2", false, "18/01/2019"));
+        this.tasks.push(new Task(1, "Task 3", false, "23/01/2019"));
+    }
 
+    getDueDateLabel(task: Task){
+        return  task.completed ? "label-success" : "label-primary";
+    }
+
+    onTaskChange(event, task){
+        console.log("Task has change");
+        // this.taskService.save(task, event.target.checked).subscribe();
+    }
 }
